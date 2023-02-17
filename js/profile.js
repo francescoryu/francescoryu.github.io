133a6ac53f07e43ed6ee770c2a6bac03862845a0
francescoryu <francescoryu04@gmail.com>
2023-02-18 00:46:52 +0100

    some changes

---
+++ js/profile.js
@@ -0,0 +1,21 @@
+window.onload = function onloadDoc() {
+    document.getElementById("saveButton").disabled = true;
+}
+
+function makeUserInfoEditable() {
+    document.getElementById("email").contentEditable = "true";
+    document.getElementById("username").contentEditable = "true";
+    document.getElementById("class").contentEditable = "true";
+    document.getElementById("points").contentEditable = "true";
+    document.getElementById("saveButton").disabled = false;
+    document.getElementById("editButton").disabled = true;
+}
+
+function makeUserInfoUneditable() {
+    document.getElementById("email").contentEditable = "false";
+    document.getElementById("username").contentEditable = "false";
+    document.getElementById("class").contentEditable = "false";
+    document.getElementById("points").contentEditable = "false";
+    document.getElementById("saveButton").disabled = true;
+    document.getElementById("editButton").disabled = false;
+}
